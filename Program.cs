ba7866285793650e3d582f6f5a08171f9efc4514
Aleksandr <72871900poker@gmail.com>
2023-02-25 19:51:43 +0300

    add task solution

--- Program.cs
+++ Program.cs
@@ -1,6 +1,49 @@
-﻿
+﻿string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
 
-for()
-Console.WriteLine("Введите элементы массива");
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+            Console.Write(array[i]);
+        else    
+        Console.Write(array[i] + " ");
+    }
+}
 
+string[] NewArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+        else Array.Resize(ref newArray, array.Length - 1); 
+    }
+    
+    return newArray;
+}
 
+Console.Write("Input a number elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreateArray(n);
+Console.Write("This is current array: ");
+ShowArray(myArray);
+Console.WriteLine();
+Console.Write("This is corrected array: ");
+string[] newArray = NewArray(myArray);
+ShowArray(newArray);
